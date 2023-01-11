17e2941340a330cc22f1e05530b6e37407d17ba4
jblackman42 <jblackman@pureheart.org>
2023-01-11 15:54:55 -0700

    Email template

--- routes/mp.js
+++ routes/mp.js
@@ -104,7 +104,102 @@ router.post('/confirmation-email', async (req, res) => {
     if (!access_token) await authorize();
     
     const {Recipient_Name, Recipient_Email, Start_Date, End_Date, Community_ID} = req.body;
-    console.log(req.body)
+    
+    axios({
+        method: 'post',
+        url: 'https://my.pureheart.org/ministryplatformapi/messages',
+        headers: {
+            'Authorization': `Bearer ${access_token}`,
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "FromAddress": { "DisplayName": "We Pray All Day", "Address": "noreply@pureheart.org" },
+            "ToAddresses": 
+            [ 
+                { "DisplayName": Recipient_Name, "Address": Recipient_Email }
+            ],
+            "ReplyToAddress": { "DisplayName": "noreply@pureheart.org", "Address": "noreply@pureheart.org" },
+            "Subject": `🙏 We Pray All Day`,
+            "Body": `<style>
+                        body, html {
+                            margin: 0;
+                            padding: 0;
+                            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                        }
+                        .container {
+                            max-width: 768px;
+                            margin: 0 auto;
+                            background-color: #f1f2f6;
+                        }
+                        .container .img-container {
+                            background-color: #0eb2f1;
+                            color: white;
+                            display: grid;
+                            place-items: center;
+                            font-size: 1.2rem;
+                            padding: 1rem;
+                        }
+                        .container .img-container img {
+                            width: 384px;
+                            max-width: 90%;
+                            margin: 0 auto;
+                        }
+                        .container .img-container p {
+                            margin: 0;
+                            font-weight: bold;
+                            text-align: center;
+                        }
+                        
+                        .container .content {
+                            max-width: 90%;
+                            margin: 0 auto;
+                            padding: 1rem;
+                        }
+                        .container .content p {
+                            margin: 0;
+                        }
+                        .container .btn-container {
+                            width: 100%;
+                            display: flex;
+                            justify-content: center;
+                        }
+                        .container button {
+                            font-size: 1rem;
+                            font-weight: bold;
+                            border: none;
+                            color: white;
+                            background-color: #0eb2f1;
+                            padding: .5rem 1rem;
+                            border-radius: 4px;
+                            cursor: pointer;
+                        }
+                    </style>
+                    <body>
+                        <div class="container">
+                            <div class="img-container">
+                                <img src="https://www.pureheart.org/wp-content/uploads/2023/01/logo.png" alt="We Pray All Day">
+                                <p>Thanks for signing up to pray</p>
+                            </div>
+                            <div class="content">
+                                <p>Hi {Name},</p>
+                                <br>
+                                <p>We are so honored that you would pray with us! We have high expectations that God is going to do immeasurably more than we could ever seek ask or imagine!</p>
+                                <br>
+                                <p>To help you remember your prayer time, add this to your calendar! It's simple all you gotta do is click it and accept 😁</p>
+                                <br>
+                                <p>We will text you 5 minutes before your scheduled time of prayer with more information.</p>
+                                <br>
+                                <p style="text-align: center;">Wed Jan 11th 2023</p>
+                                <p style="text-align: center;">5:00 pm - 5:59 pm</p>
+                                <br>
+                                <div class="btn-container">
+                                    <button>Add to Calendar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </body>`
+        }
+    })
 
     res.status(200).end();
 })
